ad6877b1d55ca2eef860f2d6d68f4157dff7286d
Axel DG <alexander18br17@gmail.com>
2026-05-06 11:03:09 -0500

    Semana_5.2 06/05/2026

--- modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
+++ modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.ute.compose.ui.material3.Paso01SumaScreen
+import com.ute.compose.ui.material3.Paso01TextFieldScreen
+import com.ute.compose.ui.material3.Paso02CardScreen
 import com.ute.compose.ui.screens.*
 
 class MainActivity : ComponentActivity() {
@@ -13,13 +16,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 // ◀ CAMBIA AQUÍ para probar cada paso:
-                // S01_SaludoScreen()
-                S02_TextScreen()
-                // Paso02_CardScreen()
+                // S01SaludoScreen()
+                // S02TextScreen()
+                // S03ButtonScreen()
+                // S04LayoutScreen()
+                // S05ModifierScreen()
+                // S06EstadoScreen()
+                // S07StateHoistingScreen()
+                // S08BienvenidaScreen()
+
+
+                // Componetes de Material 3:
+                // Paso01TextFieldScreen()
+                Paso01SumaScreen()
+                //Paso02CardScreen()
                 // Paso03_LazyColumnScreen()
                 // Paso04_ScaffoldScreen()
                 // Paso05_NavBarScreen()
-                //Paso06_DialogosScreen()   // ← paso activo
+                // Paso06_DialogosScreen()
             }
         }
     }
