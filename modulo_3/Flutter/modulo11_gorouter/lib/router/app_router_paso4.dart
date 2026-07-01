e65e41b18dc9bfcc364927b8b36f31119998963b
Axel-25-dg <alexander18br17@gmail.com>
2026-07-01 06:04:06 -0500

    Modulo_10_and_11

---
+++ modulo_3/Flutter/modulo11_gorouter/lib/router/app_router_paso4.dart
@@ -0,0 +1,5 @@
+// lib/router/app_router_paso4.dart
+import 'app_router.dart';
+
+// Placeholder for Paso 4
+final appRouterPaso4 = appRouter;
---
+++ modulo_3/Flutter/modulo11_gorouter/lib/router/app_router_paso5.dart
@@ -0,0 +1,7 @@
+// lib/router/app_router_paso5.dart
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'app_router.dart';
+
+// Placeholder for Paso 5
+GoRouter appRouterPaso5(BuildContext context) => appRouter;
